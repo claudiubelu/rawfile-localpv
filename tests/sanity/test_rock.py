2356ea2efae78c60a44549e3c397a826f5d519ad
Claudiu Belu <cbelu@cloudbasesolutions.com>
2025-01-10 09:04:37 +0200

    Bump rawfile-localpv and rock version (#21)
    Signed-off-by: Claudiu Belu <claudiu.belu@canonical.com>

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -6,7 +6,7 @@ import pytest
 from k8s_test_harness.util import docker_util, env_util
 
 
-@pytest.mark.parametrize("image_version", ["0.8.0"])
+@pytest.mark.parametrize("image_version", ["0.8.1"])
 def test_sanity(image_version):
     rock = env_util.get_build_meta_info_for_rock_version(
         "rawfile-localpv", image_version, "amd64"
